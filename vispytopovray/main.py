66d7a4b9facf86cf3d3bf6d6b72c77b6c65827e6
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-04-06 23:24:58 +0100

    Rearranged functions and doc

--- vispytopovray/main.py
+++ vispytopovray/main.py
@@ -13,12 +13,49 @@ from vispy.visuals import MeshVisual
 from vispy.scene.canvas import SceneCanvas
 from vispy.scene import ViewBox
 
+
+def export_to_povray(obj, filen):
+    '''Converts a vispy SceneCanvas or ViewBox to POVRay code.
+
+    If a SceneCanvas is passed, this works in a hacky way by searching
+    the canvas.central_widget for a ViewBox child.
+
+    With a ViewBox located, the mesh data for each of its child
+    MeshVisuals is written to a POVRay file (via a jinja2
+    template). This does *not* draw all kinds of vispy visual
+    and does *not* pick up all mesh parameters. Specifically, the
+    POVRay mesh includes the original mesh vertices, vertex_colors,
+    vertex_normals and faces.
+
+    Parameters
+    ----------
+    obj : vispy.scene.SceneCanvas or vispy.scene.ViewBox
+        The SceneCanvas or ViewBox to export to POVRay.
+    filen : str
+        The filen at which to write the POVRay output file.
+
+    '''
+    if isinstance(obj, SceneCanvas):
+        kwargs = _scenecanvas_to_kwargs(obj)
+    elif isinstance(obj, ViewBox):
+        kwargs = _viewbox_to_kwargs(obj)
+        kwargs.update({'bgcolor': (1.0, 1.0, 1.0),
+                       'ambient': (1.0, 1.0, 1.0)})
+
+    env = Environment(loader=FileSystemLoader(directory))
+    template = env.get_template('povray.pov')
+
+    with open(filen, 'w') as fileh:
+        fileh.write(template.render(**kwargs))
+
+
 POVRayMeshData = namedtuple('POVRayMeshData', ['vertex_vectors',
                                                'normal_vectors',
                                                'vertex_colors',
                                                'faces',])
 
-def mesh_to_povray(mesh, cam_inv_transform=None):
+
+def _mesh_to_povray(mesh, cam_inv_transform=None):
     '''Converts a povray Mesh scene object (i.e. MeshVisual + Node)
     to a collection of extracted POVRay data.
 
@@ -50,30 +87,13 @@ def mesh_to_povray(mesh, cam_inv_transform=None):
         faces=md.get_faces())
     
 
+
 # Doesn't work with fov == 0!
 # Should use povray orthographic camera?
-def scenecanvas_to_povray(canvas, filen):
-    '''Converts a vispy SceneCanvas to POVRay code.
-
-    This works in a hacky way by searching the canvas.central_widget
-    for a ViewBox child, extracting all the Mesh visuals from
-    the first one found, and writing the mesh data for each of these
-    to a POVRay file (via a jinja2 template). This means it does *not*
-    draw all kinds of vispy visual and does *not* pick up all mesh
-    parameters.
-
-    Specifically, the POVRay mesh includes the original mesh vertices,
-    vertex_colors, vertex_normals and faces.
-
-    Parameters
-    ----------
-    canvas : vispy.scene.SceneCanvas
-        The SceneCanvas to export to POVRay.
-    filen : str
-        The filen at which to write the POVRay output file.
+def _scenecanvas_to_kwargs(canvas):
+    '''Converts a SceneCanvas to POVRay variables to write to
+    a template.
     '''
-
-    assert isinstance(canvas, SceneCanvas)
 
     kwargs = {'bgcolor': tuple(canvas.bgcolor.rgb),
               'ambient': (1.0, 1.0, 1.0)}
@@ -82,21 +102,17 @@ def scenecanvas_to_povray(canvas, filen):
     viewbox_kwargs = None
     for child in canvas.central_widget._widgets:
         if isinstance(child, ViewBox):
-            viewbox_kwargs = viewbox_to_kwargs(child)
+            viewbox_kwargs = _viewbox_to_kwargs(child)
     if viewbox_kwargs is None:
         raise ValueError('Could not find ViewBox child of canvas '
                          'central_widget')
 
     kwargs.update(viewbox_kwargs)
+
+    return kwargs
                 
-    env = Environment(loader=FileSystemLoader(directory))
-    template = env.get_template('povray.pov')
 
-    with open(filen, 'w') as fileh:
-        fileh.write(template.render(**kwargs))
-
-
-def viewbox_to_kwargs(viewbox):
+def _viewbox_to_kwargs(viewbox):
     kwargs = {}
 
     camera = viewbox.camera
@@ -107,7 +123,7 @@ def viewbox_to_kwargs(viewbox):
     meshes = [c for c in viewbox._scene._children if
               isinstance(c, MeshVisual)]
 
-    kwargs['meshes'] = [mesh_to_povray(mesh, camera.transform.inverse)
+    kwargs['meshes'] = [_mesh_to_povray(mesh, camera.transform.inverse)
                         for mesh in meshes]
 
     return kwargs
